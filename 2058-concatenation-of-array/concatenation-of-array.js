028065cb6f9d690dbedad8eb601226763fb90869
MariusReik <89257280+MariusReik@users.noreply.github.com>
2024-06-10 22:40:26 +0200

    Time: 70 ms (33.53%) | Memory: 52.4 MB (28.94%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -4,12 +4,5 @@
  */
 var getConcatenation = function(nums) {
     
-    let ans = [...nums];
-    const length = ans.length; 
-    for (let i = 0; i < length; i++)
-    {
-        ans[length + i] = nums[i];
-    }
-    return ans; 
-
-};
+    return [...nums,...nums];
+}
