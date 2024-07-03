08b05b800d71172bcd91c2be638ac98fa589ba0b
MariusReik <89257280+MariusReik@users.noreply.github.com>
2024-07-03 18:16:00 +0200

    Time: 53 ms (53.56%) | Memory: 48.5 MB (87.58%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,13 +3,25 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
+    // loop from right moving left
+    // add 1 and determin if you need to carry
+    let [carry, idx, nums] = [1, digits.length-1, [...digits]]
+
+    // as soon as you don't have to carry RETURN
+    while(carry){
+        const val = nums[idx] + carry
+        if( val === 10){
+            nums[idx] = 0
+            if(idx === 0){
+                nums.unshift(carry)
+                carry = 0
+                }
+        }else{
+            nums[idx] = val
+            carry = 0
         }
-        digits[i] = 0;
+        idx--
     }
-    digits.unshift(1);
-    return digits;
+
+    return nums
 };
