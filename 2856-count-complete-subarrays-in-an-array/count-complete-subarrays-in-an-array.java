5fe11beebe296600f8f7405790f1560bfa3e5af1
MariusReik <89257280+MariusReik@users.noreply.github.com>
2025-04-24 16:05:10 +0200

    Time: 293 ms (5.23%) | Memory: 45 MB (78.62%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,27 +1,13 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        long totalDistinct = Arrays.stream(nums).distinct().count();
-        int count = 0;
-        
-        for (int start = 0; start < nums.length; start++) {
-            Set<Integer> currentElements = new HashSet<>();
-            
-            for (int end = start; end < nums.length; end++) {
-                currentElements.add(nums[end]);
-                
-                if (currentElements.size() == totalDistinct) {
-                    count++;
-                }
-                
-                if (currentElements.size() > totalDistinct) {
-                    break;
-                }
+        int k = (int) Arrays.stream(nums).distinct().count(), res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> st = new HashSet<>();
+            for (int j = i; j < nums.length; j++) {
+                st.add(nums[j]);
+                if (st.size() == k) res++;
             }
         }
-        
-        return count;
+        return res;
     }
 }
